08a3a4e4e94d58554280af719258778fe28870b6
Maksim Krivenchuk <123389807+Maxiimkka@users.noreply.github.com>
2023-11-26 02:06:01 +0300

    Add files via upload

---
+++ OIBlr4.0/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using SecurityLibrary.DiffieHellman;
+using SecurityLibrary.AES;
+using SecurityLibrary.ElGamal;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод текста для шифрования
+        Console.WriteLine("Введите текст для шифрования: ");
+        string originalText = Console.ReadLine();
+
+        // Шифрование и расшифрование с использованием RSA
+        Console.WriteLine("RSA:");
+        RSAEncryption(originalText);
+
+        // Шифрование и расшифрование с использованием Диффи-Хеллман
+        Console.WriteLine("\nДиффи-Хеллман:");
+        DiffieHellmanEncryption(originalText);
+
+        // Шифрование и расшифрование с использованием Эль-Гамаля
+        Console.WriteLine("\nЭль-Гамаля:");
+        ElGamalEncryption(originalText);
+    }
+
+    static void RSAEncryption(string originalText)
+    {
+        try
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                RSAParameters publicKey = rsa.ExportParameters(false);
+                RSAParameters privateKey = rsa.ExportParameters(true);
+
+                byte[] encryptedData = EncryptData(originalText, publicKey);
+                string decryptedText = DecryptData(encryptedData, privateKey);
+
+                Console.WriteLine("Исходный текст: " + originalText);
+                Console.WriteLine("Зашифрованный текст (RSA): " + BitConverter.ToString(encryptedData));
+                Console.WriteLine("Расшифрованный текст (RSA): " + decryptedText);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+    }
+
+    static byte[] EncryptData(string data, RSAParameters publicKey)
+    {
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(publicKey);
+            byte[] dataBytes = System.Text.Encoding.UTF8.GetBytes(data);
+            byte[] encryptedData = rsa.Encrypt(dataBytes, false);
+            return encryptedData;
+        }
+
+        static string DecryptData(byte[] encryptedData, RSAParameters privateKey)
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(privateKey);
+                byte[] decryptedBytes = rsa.Decrypt(encryptedData, false);
+                return System.Text.Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+
+        static void DiffieHellmanEncryption(string originalText)
+        {
+            try
+            {
+                // Создаем объект для Диффи-Хеллман
+                DiffieHellman dh = new DiffieHellman();
+
+                // Генерируем ключи
+                dh.GenerateKeys();
+                int publicKeyA = dh.GetPublicKey();
+                int sharedKeyA = dh.GetSharedKey(publicKeyA);
+
+                // Шифрование и расшифрование
+                string encryptedText = dh.EncryptText(originalText, sharedKeyA);
+                string decryptedText = dh.DecryptText(encryptedText, sharedKeyA);
+
+                Console.WriteLine("Исходный текст: " + originalText);
+                Console.WriteLine("Зашифрованный текст (Диффи-Хеллман): " + encryptedText);
+                Console.WriteLine("Расшифрованный текст (Диффи-Хеллман): " + decryptedText);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+
+        static void ElGamalEncryption(string originalText)
+        {
+            try
+            {
+                // Создаем объект для Эль-Гамаля
+                ElGamal elGamal = new ElGamal();
+
+                // Генерируем ключи
+                elGamal.GenerateKeys();
+                int publicKeyY = elGamal.GetPublicKeyY();
+                int privateKeyX = elGamal.GetPrivateKeyX();
+
+                // Шифрование и расшифрование
+                Tuple<int, int> encryptedText = elGamal.EncryptText(originalText, publicKeyY);
+                string decryptedText = elGamal.DecryptText(encryptedText, privateKeyX);
+
+                Console.WriteLine("Исходный текст: " + originalText);
+                Console.WriteLine("Зашифрованный текст (Эль-Гамаля): C1=" + encryptedText.Item1 + ", C2=" + encryptedText.Item2);
+                Console.WriteLine("Расшифрованный текст (Эль-Гамаля): " + decryptedText);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ algorithms RSA,Diffi-Hellman,El-Gamal/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        string fullName = "Krivenchuk Maxim";
+
+        // Генерация ключей RSA
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            // Шифрование ФИО с использованием RSA
+            byte[] encryptedData = RSAEncrypt(fullName, rsa.ExportParameters(false));
+
+            // Дешифрование ФИО с использованием RSA
+            string decryptedData = RSADecrypt(encryptedData, rsa.ExportParameters(true));
+
+            Console.WriteLine("Шифрование и дешифрование ФИО с использованием RSA:");
+            Console.WriteLine("Исходное ФИО: " + fullName);
+            Console.WriteLine("Зашифрованное ФИО: " + Convert.ToBase64String(encryptedData));
+            Console.WriteLine("Расшифрованное ФИО: " + decryptedData);
+            Console.WriteLine();
+        }
+
+        // Генерация ключей Диффи-Хеллмана
+        using (var dh = new ECDiffieHellmanCng())
+        {
+            // Шифрование ФИО с использованием Диффи-Хеллмана
+            byte[] encryptedData = DHEncrypt(fullName, dh);
+
+            // Дешифрование ФИО с использованием Диффи-Хеллмана
+            string decryptedData = DHDecrypt(encryptedData, dh);
+
+            Console.WriteLine("Шифрование и дешифрование ФИО с использованием Диффи-Хеллмана:");
+            Console.WriteLine("Исходное ФИО: " + fullName);
+            Console.WriteLine("Зашифрованное ФИО: " + Convert.ToBase64String(encryptedData));
+            Console.WriteLine("Расшифрованное ФИО: " + decryptedData);
+            Console.WriteLine();
+        }
+
+        // Генерация ключей Эль-Гамаля
+        using (var elGamal = new ECDiffieHellmanCng())
+        {
+            // Шифрование ФИО с использованием Эль-Гамаля
+            byte[] encryptedData = ElGamalEncrypt(fullName, elGamal);
+
+            // Дешифрование ФИО с использованием Эль-Гамаля
+            string decryptedData = ElGamalDecrypt(encryptedData, elGamal);
+
+            Console.WriteLine("Шифрование и дешифрование ФИО с использованием Эль-Гамаля:");
+            Console.WriteLine("Исходное ФИО: " + fullName);
+            Console.WriteLine("Зашифрованное ФИО: " + Convert.ToBase64String(encryptedData));
+            Console.WriteLine("Расшифрованное ФИО: " + decryptedData);
+        }
+        Console.ReadLine();
+    }
+
+    // Метод для шифрования ФИО с использованием RSA
+    static byte[] RSAEncrypt(string data, RSAParameters rsaParameters)
+    {
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(rsaParameters);
+            return rsa.Encrypt(System.Text.Encoding.UTF8.GetBytes(data), true);
+        }
+    }
+
+    // Метод для дешифрования ФИО с использованием RSA
+    static string RSADecrypt(byte[] encryptedData, RSAParameters rsaParameters)
+    {
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(rsaParameters);
+            byte[] decryptedData = rsa.Decrypt(encryptedData, true);
+            return System.Text.Encoding.UTF8.GetString(decryptedData);
+        }
+    }
+
+    // Метод для шифрования ФИО с использованием Диффи-Хеллмана
+    static byte[] DHEncrypt(string data, ECDiffieHellmanCng dh)
+    {
+        byte[] publicKey = dh.PublicKey.ToByteArray();
+        byte[] sharedKey = dh.DeriveKeyMaterial(CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob));
+        byte[] encryptedData;
+
+        using (var aes = new AesCryptoServiceProvider())
+        {
+            aes.Key = sharedKey;
+            aes.GenerateIV();
+
+            using (var encryptor = aes.CreateEncryptor())
+            using (var memoryStream = new System.IO.MemoryStream())
+            {
+                // Записываем IV в начало потока
+                memoryStream.Write(aes.IV, 0, aes.IV.Length);
+
+                using (var cryptoStream = new System.Security.Cryptography.CryptoStream(memoryStream, encryptor, System.Security.Cryptography.CryptoStreamMode.Write))
+                using (var streamWriter = new System.IO.StreamWriter(cryptoStream))
+                {
+                    streamWriter.Write(data);
+                }
+
+                encryptedData = memoryStream.ToArray();
+            }
+        }
+
+        return encryptedData;
+    }
+
+    // Метод для дешифрования ФИО с использованием Диффи-Хеллмана
+    static string DHDecrypt(byte[] encryptedData, ECDiffieHellmanCng dh)
+    {
+        byte[] publicKey = dh.PublicKey.ToByteArray();
+        byte[] sharedKey = dh.DeriveKeyMaterial(CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob));
+        byte[] iv = new byte[16];
+
+        using (var aes = new AesCryptoServiceProvider())
+        {
+            aes.Key = sharedKey;
+
+            // Читаем IV из начала зашифрованных данных
+            Array.Copy(encryptedData, iv, iv.Length);
+            aes.IV = iv;
+
+            using (var decryptor = aes.CreateDecryptor())
+            using (var memoryStream = new System.IO.MemoryStream(encryptedData, iv.Length, encryptedData.Length - iv.Length))
+            using (var cryptoStream = new System.Security.Cryptography.CryptoStream(memoryStream, decryptor, System.Security.Cryptography.CryptoStreamMode.Read))
+            using (var streamReader = new System.IO.StreamReader(cryptoStream))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+
+    // Метод для шифрования ФИО с использованием Эль-Гамаля
+    static byte[] ElGamalEncrypt(string data, ECDiffieHellmanCng elGamal)
+    {
+        byte[] publicKey = elGamal.PublicKey.ToByteArray();
+        byte[] sharedKey = elGamal.DeriveKeyMaterial(CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob));
+        byte[] encryptedData;
+
+        using (var aes = new AesCryptoServiceProvider())
+        {
+            aes.Key = sharedKey;
+            aes.GenerateIV();
+
+            using (var encryptor = aes.CreateEncryptor())
+            using (var memoryStream = new System.IO.MemoryStream())
+            {
+                // Записываем IV в начало потока
+                memoryStream.Write(aes.IV, 0, aes.IV.Length);
+
+                using (var cryptoStream = new System.Security.Cryptography.CryptoStream(memoryStream, encryptor, System.Security.Cryptography.CryptoStreamMode.Write))
+                using (var streamWriter = new System.IO.StreamWriter(cryptoStream))
+                {
+                    streamWriter.Write(data);
+                }
+
+                encryptedData = memoryStream.ToArray();
+            }
+        }
+
+        return encryptedData;
+    }
+
+    // Метод для дешифрования ФИО с использованием Эль-Гамаля
+    static string ElGamalDecrypt(byte[] encryptedData, ECDiffieHellmanCng elGamal)
+    {
+        byte[] publicKey = elGamal.PublicKey.ToByteArray();
+        byte[] sharedKey = elGamal.DeriveKeyMaterial(CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob));
+        byte[] iv = new byte[16];
+
+        using (var aes = new AesCryptoServiceProvider())
+        {
+            aes.Key = sharedKey;
+
+            // Читаем IV из начала зашифрованных данных
+            Array.Copy(encryptedData, iv, iv.Length);
+            aes.IV = iv;
+
+            using (var decryptor = aes.CreateDecryptor())
+            using (var memoryStream = new System.IO.MemoryStream(encryptedData, iv.Length, encryptedData.Length - iv.Length))
+            using (var cryptoStream = new System.Security.Cryptography.CryptoStream(memoryStream, decryptor, System.Security.Cryptography.CryptoStreamMode.Read))
+            using (var streamReader = new System.IO.StreamReader(cryptoStream))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
